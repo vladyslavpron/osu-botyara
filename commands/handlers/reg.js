85280bb533ca0198b758d813a111cb81593463fa
unknown <vladyslavpron@gmail.com>
2022-03-08 15:31:26 +0200

    вiдбiй тривоги

--- commands/handlers/reg.js
+++ commands/handlers/reg.js
@@ -1,10 +1,19 @@
+const User = require(".../models/userModel");
+
 async function reg(ctx) {
   const userId = ctx.message.from.id;
   console.log(ctx.message.text.indexOf(" "));
   if (ctx.message.text.indexOf(" ") === -1)
     return ctx.reply("Correct usage: /reg nickname");
-  const osuNickname = ctx.message.text.split(" ");
+  const osuNickname = ctx.message.text.split(" ")[1];
+
+  const a = await User.findOneAndUpdate(
+    { telegramId: userId },
+    { osuId: 20 },
+    { upsert: true }
+  );
   console.log(userId, osuNickname);
+  console.log(a);
   ctx.reply("/reg answer");
 }
 
