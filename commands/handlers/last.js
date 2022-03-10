1dfdbfd79619ff68c002722f5ee54c9b99546aed
vladyslavpron <vladyslavpron@gmail.com>
2022-03-10 14:11:01 +0200

    in process of implementing /last

--- commands/handlers/last.js
+++ commands/handlers/last.js
@@ -1,6 +1,45 @@
+const axios = require("axios");
 const renderLast = require("../../renderImage/renderStats");
+
 async function last(ctx) {
+  const lastScore = await getLastScore(12703752);
+
+  const user = {
+    username: lastScore[0].user.username,
+    avatarUrl: lastScore[0].user.avatar_url,
+    countryCode: lastScore[0].user.country_code,
+    supporter: lastScore[0].user.is_supporter,
+  };
+  const map = {
+    id: lastScore[0].beatmap.id,
+    status: lastScore[0].beatmap.status,
+    cs: lastScore[0].beatmap.cs,
+    bpm: lastScore[0].beatmap.bpm,
+  };
+  const play = {
+    mods: lastScore[0].mods,
+    score: lastScore[0].score,
+    rank: lastScore[0].rank,
+    combo: lastScore[0].max_combo,
+    date: lastScore[0].created_at,
+  };
+  console.log(user, map, play);
+  const lastImage = await renderLast(user, map, play);
   ctx.reply("/last handled!");
 }
 
 module.exports = last;
+
+async function getLastScore(user) {
+  return await axios
+    .get(
+      `https://osu.ppy.sh/api/v2/users/${user}/scores/recent?include_fails=1&limit=1`,
+      {
+        headers: {
+          Authorization: `Bearer ${process.env.BEARER}`,
+        },
+      }
+    )
+    .then((res) => res.data)
+    .catch((err) => console.log(err.response));
+}
