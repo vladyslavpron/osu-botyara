2e7aadd811adbb51f4dd1b937a067a93ad6e66ad
vladyslavpron <vladyslavpron@gmail.com>
2022-03-13 12:53:57 +0200

    Implemented offset for /last

--- commands/handlers/last.js
+++ commands/handlers/last.js
@@ -7,19 +7,25 @@ const buildObjUserPlayMap = require("./../../utils/buildObjUserPlayMap");
 async function last(ctx) {
   const start = Date.now();
 
-  let osuId;
+  let osuId, offset, username;
   const userId = ctx.message.from.id;
 
-  if (ctx.message.text.indexOf(" ") === -1)
-    osuId = (await User.findOne({ telegramId: userId })).osuId;
-  else osuId = ctx.message.text.split(" ").slice(1).join(" ");
+  const command = ctx.message.text.split(" ");
+  if (isFinite(command[1])) {
+    offset = parseInt(command[1]);
+    command.splice(0, 2);
+    username = command.join(" ");
+  } else username = command.slice(1, command.length).join(" ");
+
+  if (!username) osuId = (await User.findOne({ telegramId: userId })).osuId;
+  else osuId = username;
 
   if (!osuId) return ctx.reply("User not found");
   const userProfile = await getUser(osuId);
   osuId = userProfile?.id;
   if (!osuId) return ctx.reply("User not found");
 
-  const lastScore = await getLastScore(osuId);
+  const lastScore = await getLastScore(osuId, offset);
   if (!lastScore.length)
     return ctx.reply("Can't find recent plays for this user");
 
@@ -38,10 +44,10 @@ async function last(ctx) {
 
 module.exports = last;
 
-async function getLastScore(user) {
+async function getLastScore(user, offset) {
   return await axios
     .get(
-      `https://osu.ppy.sh/api/v2/users/${user}/scores/recent?include_fails=1&limit=1&mode=osu`,
+      `https://osu.ppy.sh/api/v2/users/${user}/scores/recent?include_fails=1&limit=1&offset=${offset}&mode=osu`,
       {
         headers: {
           Authorization: `Bearer ${process.env.BEARER}`,
