1e858f725f9c422e266a59c722761914aa9dd309
unknown <vladyslavpron@gmail.com>
2022-03-19 23:15:25 +0200

    Implemented auth for osu! api

--- index.js
+++ index.js
@@ -12,4 +12,22 @@ mongoose
   .catch((err) => console.log(err));
 //
 
+async function auth() {
+  await axios
+    .post("https://osu.ppy.sh/oauth/token", {
+      client_id: process.env.CLIENT_ID,
+      client_secret: process.env.CLIENT_SECRET,
+      grant_type: "client_credentials",
+      scope: "public",
+    })
+    .then((res) => {
+      console.log("got new osu token");
+      process.env.BEARER = res.data.access_token;
+    })
+    .catch((err) => console.log(err));
+}
+// get new bearer on start and every 22h
+auth();
+setInterval(auth, 1000 * 60 * 60 * 22);
+
 const commands = require("./commands/commands");
